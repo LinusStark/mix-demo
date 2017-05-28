e2589549722d57f434c183a7bc36b9cf6d70da19
LinusStark <454646793@qq.com>
2017-05-28 13:42:35 +0800

    first commit

---
+++ Mix/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  Mix
+//
+//  Created by 李 冬岐 on 17/5/28.
+//  Copyright © 2017年 李 冬岐. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@end
